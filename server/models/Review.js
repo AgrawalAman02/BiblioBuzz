168d58c3173c2d9f20f62b92dbef1129993b5f7c
AgrawalAman02 <amanhzb2003@gmail.com>
2025-04-16 16:22:19 +0530

    initial commit

---
+++ server/models/Review.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book',
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number,
+    default: 0
+  }
+}, { timestamps: true });
+
+// Ensure a user can only review a book once
+ReviewSchema.index({ user: 1, book: 1 }, { unique: true });
+
+const Review = mongoose.model('Review', ReviewSchema);
+module.exports = Review;
